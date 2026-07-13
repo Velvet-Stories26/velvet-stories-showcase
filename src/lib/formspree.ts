9dd6b4f72e2ce64d83ea958ad3b4a6f227acbc8e
Velvet-Stories26 <sinamikas926@gmail.com>
2026-07-13 17:46:43 +0530

    form

--- src/lib/formspree.ts
+++ src/lib/formspree.ts
@@ -1,2 +1,5 @@
-// Paste your Formspree endpoint here (example: https://formspree.io/f/xxxxxxxx)
-export const FORMSPREE_ENDPOINT = "https://form.typeform.com/to/L8jHxPXH";
+// Web3Forms Configuration
+// Get your Access Key from https://web3forms.com
+// Paste your Web3Forms Access Key here (example: xxxxxxxxxxxxxxxxxxxxxxxx)
+export const WEB3FORMS_ACCESS_KEY = "d80659a3-4757-4ebf-95f3-bccb4db92c00";
+export const WEB3FORMS_ENDPOINT = "https://api.web3forms.com/submit";
